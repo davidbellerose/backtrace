9f70e92a4e01f9421837fb47697de8108562b582
daveyedgar <62836024+daveyedgar@users.noreply.github.com>
2022-12-28 14:57:53 -0500

    code complete

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,19 +1,28 @@
 // Add event listener
-
+document.getElementById("btnSubmit").addEventListener("click", getValue);
 
 // Get string from the input field
 // Controller function
 function getValue(){
-
+  document.getElementById("alert").classList.add("invisible");
+  let userString = document.getElementById("userString").value;
+  let revString = reverseString(userString);
+  displayString(revString);
 };
 
 // Revers the string
 // Logic function
-function reverseString(){
-
+function reverseString(userString){
+  let revString = [];
+  for(let i = userString.length - 1; i >= 0; i--){
+    // revString.push(revString[i]);
+    revString += userString[i];
+  }
+  return revString;
 };
 // Display the reversed string on the page
 // View function
-function displayString(){
-
+function displayString(revString){
+  document.getElementById("msg").innerHTML = `The reversed string is ${revString}`;
+  document.getElementById("alert").classList.remove("invisible");
 };
